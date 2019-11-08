cb5a5c8c6bb2646ff27c7afb8fac386426f0f07f
misbahmunirinalkhafadh <misbah8135@students.amikom.ac.id>
2019-11-08 17:00:35 +0700

    update login

--- app/src/main/java/com/mii/assetmanagement/MainActivity.java
+++ app/src/main/java/com/mii/assetmanagement/MainActivity.java
@@ -1,15 +1,42 @@
 package com.mii.assetmanagement;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView iv_profile;
+    TextView tvResultName;
+
+    SharedPrefManager sharedPrefManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initComponent();
+
+        sharedPrefManager = new SharedPrefManager(this);
+        tvResultName.setText(sharedPrefManager.getSPNama());
+
+        iv_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoprofile = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(gotoprofile);
+            }
+        });
+    }
+
+    private void initComponent() {
+        iv_profile = (ImageView) findViewById(R.id.img_profile);
+        tvResultName = (TextView) findViewById(R.id.tv_result_name);
     }
 
 }
