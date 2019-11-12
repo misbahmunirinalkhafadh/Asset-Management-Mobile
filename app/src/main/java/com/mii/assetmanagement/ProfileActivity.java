37f7ca1dfb74d6d63272242510901208b06a3ddd
henthreey <henthreey@gmail.com>
2019-11-12 13:51:13 +0700

    fixing error
    penambahan layout dan pembaruan main menu

--- app/src/main/java/com/mii/assetmanagement/ProfileActivity.java
+++ app/src/main/java/com/mii/assetmanagement/ProfileActivity.java
@@ -1,16 +1,43 @@
 package com.mii.assetmanagement;
 
-import androidx.appcompat.app.AppCompatActivity;
-import de.hdodenhof.circleimageview.CircleImageView;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView tvResultName;
+    Button btnLogout;
+
+    SharedPrefManager sharedPrefManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        initComponent();
+
+        sharedPrefManager = new SharedPrefManager(this);
+        tvResultName.setText(sharedPrefManager.getSPNama());
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sharedPrefManager.saveSPBoolean(SharedPrefManager.SP_SUDAH_LOGIN, false);
+                startActivity(new Intent(ProfileActivity.this, LoginActivity.class)
+                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+                finish();
+            }
+        });
+    }
+
+    private void initComponent() {
+        tvResultName = (TextView) findViewById(R.id.tv_result_name);
+        btnLogout = (Button) findViewById(R.id.btn_logout);
     }
 }
