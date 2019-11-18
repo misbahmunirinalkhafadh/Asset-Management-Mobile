10c32e81967cf00b5ee4d07ec8574b2180144304
misbahmunirinalkhafadh <misbah8135@students.amikom.ac.id>
2019-11-18 09:47:46 +0700

    Update ProfileActivity.java

--- app/src/main/java/com/mii/assetmanagement/ProfileActivity.java
+++ app/src/main/java/com/mii/assetmanagement/ProfileActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -72,18 +71,12 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
                 Log.v("remove session", "check function");
 
-//                sharedPrefManager.saveSPString(SharedPrefManager.SP_EMAIL, sharedPrefManager.getSPEmail());
                 // Shared Pref ini berfungsi untuk menjadi trigger session login
                 sharedPrefManager.saveSPBoolean(SharedPrefManager.SP_SUDAH_LOGIN, false);
-//
+
                 startActivity(new Intent(ProfileActivity.this, LoginActivity.class)
                         .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
                 finish();
-
-//                Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
-//                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-//                startActivity(intent);
-//                finish();
 
         }
     }
