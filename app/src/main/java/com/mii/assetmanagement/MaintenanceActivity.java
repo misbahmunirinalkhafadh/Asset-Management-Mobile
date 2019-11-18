b48706fa707c9beaf14038038e63d42cc17028a3
henthreey <henthreey@gmail.com>
2019-11-18 13:14:10 +0700

    Maintenance Assets

--- app/src/main/java/com/mii/assetmanagement/MaintenanceActivity.java
+++ app/src/main/java/com/mii/assetmanagement/MaintenanceActivity.java
@@ -3,6 +3,7 @@ package com.mii.assetmanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MaintenanceActivity extends AppCompatActivity {
 
