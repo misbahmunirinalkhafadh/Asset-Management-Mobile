58ce427113765cd1d38cadd4c3d13a44f05c5074
henthreey <henthreey@gmail.com>
2020-01-28 10:40:03 +0700

    update requets

---
+++ app/src/main/java/com/mii/assetmanagement/PopupActivity.java
@@ -0,0 +1,33 @@
+package com.mii.assetmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.WindowManager;
+
+public class PopupActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width * .9), (int) (height * .5));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.BOTTOM;
+        params.x = 0;
+        params.y = 0;
+
+        getWindow().setAttributes(params);
+    }
+}
