28b1d91b5879de273d2f3906cc1a1a6f805e1d56
henthreey <henthreey@gmail.com>
2020-01-28 15:06:19 +0700

    up

--- app/src/main/java/com/mii/assetmanagement/View/SearchAssetActivity.java
+++ app/src/main/java/com/mii/assetmanagement/View/SearchAssetActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.mii.assetmanagement.PopupActivity;
 import com.mii.assetmanagement.R;
 
 public class SearchAssetActivity extends AppCompatActivity {
