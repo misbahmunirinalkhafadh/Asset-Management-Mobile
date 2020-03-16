556a868bcfe5a1aacb684514ef0beaa6ebc1fbda
henthreey <henthreey@gmail.com>
2020-03-16 09:21:41 +0700

    update

---
+++ app/src/main/java/com/mii/assetmanagement/HistoryDetailMaintenanceActivity.java
@@ -0,0 +1,14 @@
+package com.mii.assetmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HistoryDetailMaintenanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_detail_maintenance);
+    }
+}
