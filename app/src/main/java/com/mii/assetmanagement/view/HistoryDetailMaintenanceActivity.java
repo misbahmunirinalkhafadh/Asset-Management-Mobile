d3a152efd805aa0d1aad90e81c63052ba9fddac5
misbahmunirinalkhafadh <misbah8135@students.amikom.ac.id>
2020-03-19 15:01:16 +0700

    update history layout

--- app/src/main/java/com/mii/assetmanagement/view/HistoryDetailMaintenanceActivity.java
+++ app/src/main/java/com/mii/assetmanagement/view/HistoryDetailMaintenanceActivity.java
@@ -1,16 +1,61 @@
 package com.mii.assetmanagement.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
 
 import com.mii.assetmanagement.R;
 
 public class HistoryDetailMaintenanceActivity extends AppCompatActivity {
+    private TextView tvIdTrans, tvProcessor, tvRam, tvHdd, tvSsd, tvOs, tvAntivirus, tvAppStd, tvCleaning, tvFunction, tvIpAddress, tvUnameLogin, tvCompName, tvReason;
     public static final String EXTRA_HISTORY = "extra_history";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history_detail_maintenance);
+        actionBar();
+        initComponent();
+    }
+
+    private void initComponent() {
+        tvIdTrans = findViewById(R.id.tv_id_request);
+        tvProcessor = findViewById(R.id.tv_processor);
+        tvRam = findViewById(R.id.tv_ram);
+        tvHdd = findViewById(R.id.tv_hdd);
+        tvSsd = findViewById(R.id.tv_ssd);
+        tvOs = findViewById(R.id.tv_os);
+        tvAntivirus = findViewById(R.id.tv_antivirus);
+        tvAppStd = findViewById(R.id.tv_app_standard);
+        tvCleaning = findViewById(R.id.tv_cleaning);
+        tvFunction = findViewById(R.id.tv_function);
+        tvIpAddress = findViewById(R.id.tv_ip_address);
+        tvUnameLogin = findViewById(R.id.tv_uname_login);
+        tvCompName = findViewById(R.id.tv_comp_name);
+    }
+
+    private void actionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        AppCompatTextView mTitleTextView = new AppCompatTextView(getApplicationContext());
+        ActionBar.LayoutParams layoutParams = new ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
+        layoutParams.gravity = Gravity.START;
+        if (actionBar != null) {
+            actionBar.setCustomView(mTitleTextView, layoutParams);
+            actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_HOME_AS_UP);
+        }
+        mTitleTextView.setText(getString(R.string.appbar_history_detail));
+        mTitleTextView.setTextAppearance(getApplicationContext(), android.R.style.TextAppearance_DeviceDefault_Large);
+        mTitleTextView.setTextColor(Color.WHITE);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
--- app/src/main/java/com/mii/assetmanagement/view/HistoryDetailRequestNewActivity.java
+++ app/src/main/java/com/mii/assetmanagement/view/HistoryDetailRequestNewActivity.java
@@ -1,16 +1,62 @@
 package com.mii.assetmanagement.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.mii.assetmanagement.R;
 
 public class HistoryDetailRequestNewActivity extends AppCompatActivity {
+
+    private TextView tvIdTrans, tvCurrentNik, tvCurrentName, tvNewNik, tvOldSerial, tvCompany, tvBranch, tvReason;
+    private RecyclerView recyclerView;
     public static final String EXTRA_HISTORY = "extra_history";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history_detail_request_new);
+        actionBar();
+        initComponent();
+
+    }
+
+    private void initComponent() {
+        tvIdTrans = findViewById(R.id.tv_id_request);
+        tvCurrentNik = findViewById(R.id.tv_currentNik);
+        tvCurrentName = findViewById(R.id.tv_currentName);
+        tvNewNik = findViewById(R.id.tv_new_nik);
+        tvOldSerial = findViewById(R.id.tv_old_serialNumber);
+        tvCompany = findViewById(R.id.tv_company);
+        tvBranch = findViewById(R.id.tv_branch);
+        tvReason = findViewById(R.id.tv_reason);
+
+        recyclerView = findViewById(R.id.rv_item_brand);
+    }
+
+    private void actionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        AppCompatTextView mTitleTextView = new AppCompatTextView(getApplicationContext());
+        ActionBar.LayoutParams layoutParams = new ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
+        layoutParams.gravity = Gravity.START;
+        if (actionBar != null) {
+            actionBar.setCustomView(mTitleTextView, layoutParams);
+            actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_HOME_AS_UP);
+        }
+        mTitleTextView.setText(getString(R.string.appbar_history_detail));
+        mTitleTextView.setTextAppearance(getApplicationContext(), android.R.style.TextAppearance_DeviceDefault_Large);
+        mTitleTextView.setTextColor(Color.WHITE);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
